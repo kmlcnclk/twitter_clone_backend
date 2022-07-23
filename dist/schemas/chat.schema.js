cfb563c2f79ea3a1c2cc9664bb45b5990d031a3e
Kamilcan Çelik <kamilcancelik5757@gmail.com>
2022-07-23 20:38:41 +0300

    .

--- dist/schemas/chat.schema.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.createChatSchema = void 0;
-var yup_1 = require("yup");
-exports.createChatSchema = (0, yup_1.object)({
-    body: (0, yup_1.object)({
-        _id: (0, yup_1.string)().required('Id is required'),
-    }),
-});
